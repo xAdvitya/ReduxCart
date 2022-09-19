ddf0d6533380307cf032be00177abfac8a3590cb
xAdvitya <advityasharma21@gmail.com>
2022-09-19 18:06:57 +0530

    adding data to firebase

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,14 +1,21 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const uiSlice=createSlice({
-    name:'ui',
-    initialState:{cardVisible:false},
-    reducers:{
-        toggle(state){
-            state.cardVisible = !state.cardVisible
-        }
-    }
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState: { cardVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cardVisible = !state.cardVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
-export const uiActions = uiSlice.actions
-export default uiSlice.reducer
+export const uiActions = uiSlice.actions;
+export default uiSlice.reducer;
